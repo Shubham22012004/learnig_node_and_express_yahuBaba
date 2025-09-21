3265a118c6e0469e45250488c655107dca2afc40
Shubham22012004 <shubhamyadavoffice123@gmail.com>
2025-09-21 19:34:31 +0530

    session tutorial

---
+++ Session_tut/index.js
@@ -0,0 +1,58 @@
+//storing session in DB-> mongoDB or MySQL
+
+import express from 'express';
+import MongoStore from 'connect-mongo';
+
+const app = express();
+import session from 'express-session';
+
+//middleware of session
+app.use(session({
+    secret:"any_Value",
+    resave:false,
+    saveUnitialized : false,
+    store : MongoStore.create({
+        mongoUrl :'mongodb://127.0.0.1/sessiondb',
+        collectionName : 'mysession' // if you not give collection name then it will automatically create
+    }), //this add to store session in db
+    cookie:{maxAge:1000*60*60*24} //you can use ''ttl'' key in store time and can remove cookie
+}))
+
+app.get('/',(req,res)=>{
+    res.send("<h1>no session created yet</h1>");
+})
+
+//session creation 
+/*
+this session is fromed  at system RAM, but the RAM has less capacity so most developer try to save it in mongoDB using package ''connect-mongo'' to reduce the overhead at server
+*/
+
+app.get('/set-session', (req,res)=>{
+    req.session.any_key = "world"; //by default session value store for 24 hrs
+    res.send("<h1>  session created </h1>")
+})
+
+//read session at server -> we can read the session value at any page or any route
+app.get('/get-session',(req,res)=>{
+    if(req.session.any_key){
+        res.send(`<h1> username from session is :${req.session.any_key}`);
+    }else{
+        res.send("no session value founnd");
+    }
+})
+
+//destroy session
+
+app.get('/destroy',(req,res)=>{
+    
+    req.session.destroy((err)=>{
+        if(err){
+            res.status(500).send('failed to destroy session');
+        }
+        res.send('session destroy successfully');
+    })
+})
+
+app.listen(5000,()=>{
+    console.log(`server is running on 5000`)
+})
